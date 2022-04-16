5ce11155cc1ac35baca984dd06ad4d696aa38460
atestee <ester@atlasova.cz>
2022-04-16 17:58:46 +0200

    Modified main.py for testing FinishedJobController

--- main.py
+++ main.py
@@ -11,31 +11,31 @@ api = Api(app)
 CORS(app)
 
 
-class AvailableCitiesController(Resource):
-    f = open('data/availableCities.json')
-    availableCities = json.load(f)
+# class AvailableCitiesController(Resource):
+#     f = open('data/availableCities.json')
+#     availableCities = json.load(f)
+#
+#     def get(self):
+#         return self.availableCities
+#
+#
+# class CityModelController(Resource):
+#     parser = reqparse.RequestParser()
+#     parser.add_argument("selectedCity")
+#     parser.add_argument("isCheck")
+#     parser.add_argument("isCheckAll")
+#
+#     cityCenterCoords = json.load(open('data/cityCoordinates.json'))
+#
+#     def get(self, selectedCity):
+#         args = self.parser.parse_args()
+#         selected_city_model_path = os.path.join("data", "cityModels", selectedCity + ".json")
+#         f = open(selected_city_model_path)
+#         city_model = json.load(f)
+#         return city_model
 
-    def get(self):
-        return self.availableCities
 
-
-class CityModelController(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument("selectedCity")
-    parser.add_argument("isCheck")
-    parser.add_argument("isCheckAll")
-
-    cityCenterCoords = json.load(open('data/cityCoordinates.json'))
-
-    def get(self, selectedCity):
-        args = self.parser.parse_args()
-        selected_city_model_path = os.path.join("data", "cityModels", selectedCity + ".json")
-        f = open(selected_city_model_path)
-        city_model = json.load(f)
-        return city_model
-
-
-class JobController(Resource):
+class FinishedJob(Resource):
     def get(self, job_id):
         http = urllib3.PoolManager()
         url = 'http://localhost:5000/v0/clusters/' + job_id + '/get-result'
@@ -55,9 +55,9 @@ class JobController(Resource):
         return job_id + " received"
 
 
-api.add_resource(AvailableCitiesController, '/api/cities/')
-api.add_resource(CityModelController, '/api/city-model/<selectedCity>')
-api.add_resource(JobController, '/api/job/<job_id>')
+# api.add_resource(AvailableCitiesController, '/api/cities/')
+# api.add_resource(CityModelController, '/api/city-model/<selectedCity>')
+api.add_resource(FinishedJob, '/api/finished-job/<job_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
